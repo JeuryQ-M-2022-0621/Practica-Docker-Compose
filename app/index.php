66cd0de6481968fc2b7f732d0aac57700378c20a
JeuryQ-M-2022-0621 <jeuryq3@gmail.com>
2023-11-12 20:49:09 -0400

    mi primer commit

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1 >Practica de Docker Compose</h1>
+
+
+<?php
+        echo "<h2>Jeury Quezada</h2>";
+        echo "<h2>2022-0621</h2>";
+    ?>
+    
+</body>
+</html>
